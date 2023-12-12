9a4bc4c5ae1a2e5e5fee2af377fca63a18e84f29
anetigor <114143140+anetigor@users.noreply.github.com>
2023-12-12 19:18:40 +0100

    Add files via upload

---
+++ dane.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 12, 2023 at 06:40 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sigma`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `dane`
+--
+
+CREATE TABLE `dane` (
+  `id` int(11) NOT NULL,
+  `imie` varchar(50) NOT NULL,
+  `nazwisko` varchar(50) NOT NULL,
+  `data_urodzenia` date NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefon` varchar(15) NOT NULL,
+  `wojewodztwo` varchar(50) NOT NULL,
+  `plec` varchar(10) NOT NULL,
+  `zgoda_newsletter` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dane`
+--
+
+INSERT INTO `dane` (`id`, `imie`, `nazwisko`, `data_urodzenia`, `email`, `telefon`, `wojewodztwo`, `plec`, `zgoda_newsletter`) VALUES
+(4, 'Szpec', 'Tryhard', '2023-12-15', 'szpecus@gmail.com', '333444222', 'Pomorskie', 'male', 1);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `dane`
+--
+ALTER TABLE `dane`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dane`
+--
+ALTER TABLE `dane`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
